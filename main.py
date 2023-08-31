f7d22cbbcb92534e2e5414adb5c02055fe3f0552
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-08-31 14:42:35 +0300

    tzinfo

--- main.py
+++ main.py
@@ -22,34 +22,31 @@ from webdriver_manager.chrome import ChromeDriverManager
 import schedule
 
 
-# service = FirefoxService(GeckoDriverManager().install())
-
 engine = create_engine(os.environ.get('DATABASE_URL'), echo=False)
 session = sessionmaker(bind=engine)()
 
-
 SOURCE_URL = os.environ.get('SOURCE_URL')
-
-# options = Options()
-# options.set_preference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1")
-# options.add_argument('--headless')
-
-
-# Set up Chrome WebDriver
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1")
-chrome_options.add_argument('--headless')
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
-prefs = {"profile.managed_default_content_settings.images": 2}
-# chrome_options.headless = True
-
-chrome_options.add_experimental_option("prefs", prefs)
-
 BASE_DIR = os.getcwd()
 
-chrome_driver = os.path.join(BASE_DIR, 'chromedriver')
-os.chmod(chrome_driver, 0o755)
+
+if os.name == 'nt':
+    options = Options()
+    options.set_preference("general.useragent.override", "Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1")
+    options.add_argument('--headless')
+else:
+
+    # Set up Chrome WebDriver
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("user-agent=Mozilla/5.0 (iPhone; CPU iPhone OS 14_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/14.0.3 Mobile/15E148 Safari/604.1")
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    prefs = {"profile.managed_default_content_settings.images": 2}
+    # chrome_options.headless = True
+
+    chrome_options.add_experimental_option("prefs", prefs)
+    chrome_driver = os.path.join(BASE_DIR, 'chromedriver')
+    os.chmod(chrome_driver, 0o755)
 
 
 def update_stock_price(stock):
@@ -58,8 +55,10 @@ def update_stock_price(stock):
     try:
 
         # binary = FirefoxBinary(os.getenv('FIREFOX_PATH'))
-        # bw = webdriver.Firefox(options=options, service=service)
-        bw = webdriver.Chrome(options=chrome_options, service=Service(executable_path=chrome_driver))
+        if os.name == 'nt':
+            bw = webdriver.Firefox(options=options)
+        else:
+            bw = webdriver.Chrome(options=chrome_options, service=Service(executable_path=chrome_driver))
         bw.get(target)
         bw.set_window_rect(width=320, height=480)
         time.sleep(5)
@@ -77,7 +76,6 @@ def update_stock_price(stock):
             percentage_change_send_to_log(percentage_change, float(price), stock, float(price) >= stock.current_price)
         stock.current_price = float(price)
         session.commit()
-
 
         try:
             if len(logger.discord.get_embeds()) > 0:
@@ -146,11 +144,14 @@ def stock_available_flower():
 
 
 def send_wake_log():
+    from time import gmtime, strftime
     logger.construct(
         title='Service is woke',
         metadata=f'name {os.name}'
+                 f'{strftime("%z", gmtime())}'
     )
     logger.send()
+    logger.discord.delete()
 
 
 def main():
@@ -167,6 +168,7 @@ def main():
     # stock available flow
     schedule.every().day.at(f'{10:02}:50').do(stock_available_flower)
 
+    print(schedule)
     while True:
         schedule.run_pending()
         time.sleep(1)
