fda99017bb7a53a3334a3bf649fca42d14deb740
F4ruk.seker <ahmetfaruk731@gmail.com>
2023-09-04 14:47:34 +0300

    get BW_PATH

--- main.py
+++ main.py
@@ -172,6 +172,7 @@ def send_wake_log(*args, **kwargs):
         metadata=f'os name {os.name} - tz {strftime("%z", gmtime())}\n'
                  f'Tasks:\n{job_text}'
                  f'{os.environ.get("BW_PATH", "BW_PATH")}'
+                 f'{os.environ.get("BW_PATHQ", "BW_PATHQ")}'
     )
     logger.send()
     logger.remove_embed_msg()
